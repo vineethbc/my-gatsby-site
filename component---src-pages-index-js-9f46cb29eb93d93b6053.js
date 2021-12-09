cb87fc78eb36932e2b9a2cb92750735c3a8ca48d
Vineeth B <vineethbc.dev@gmail.com>
2021-12-09 11:31:36 +0530

    Updates

---
+++ component---src-pages-index-js-9f46cb29eb93d93b6053.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[678],{704:function(e,t,a){a.r(t);var s=a(294),n={color:"#232129",padding:96,fontFamily:"-apple-system, Roboto, sans-serif, serif"};t.default=function(){return s.createElement("main",{style:n},s.createElement("title",null,"A Gatsby page"),"My New Gatsby site",s.createElement("nav",null,s.createElement("a",{href:"/my-gatsby-site/about"},"About")))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-9f46cb29eb93d93b6053.js.map
