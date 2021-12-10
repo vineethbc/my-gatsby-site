5e24975b3ac50f55d49eb84b6e7b64fa43b57e2f
Vineeth B <vineethbc.dev@gmail.com>
2021-12-11 01:39:59 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@ module.exports = {
     siteUrl: "https://vineethbc.github.io/my-gatsby-site/",
     title: "My Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
   pathPrefix: "/my-gatsby-site",
 };
